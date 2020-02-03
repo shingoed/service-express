b8f34cc19a7333d414c30b48391095f181207a88
Rachael Rice <Rachnicrice@gmail.com>
2020-02-03 09:21:24 -0800

    added application properties

--- src/main/java/com/rachnicrice/spordering/models/ApplicationUser.java
+++ src/main/java/com/rachnicrice/spordering/models/ApplicationUser.java
@@ -4,14 +4,23 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Collection;
 
 @Entity
 public class ApplicationUser implements UserDetails {
     //instance variables
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    Long id;
 
     String username;
     String password;
+
+    //constructor function
+    ApplicationUser () {};
 
 
     //instance methods
