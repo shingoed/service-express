7ad3c247cc06b5b8177f9ae6eb935ff0c8974471
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2020-02-02 20:48:56 -0800

    pulled down rachel's auth to implement my version of front-end

--- src/main/java/com/rachnicrice/spordering/models/ApplicationUser.java
+++ src/main/java/com/rachnicrice/spordering/models/ApplicationUser.java
@@ -4,10 +4,17 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Collection;
 
 @Entity
 public class ApplicationUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     //instance variables
 
     String username;
