b8f5eefffc3743c9749642b8885e9373e940bf92
Rachael Rice <Rachnicrice@gmail.com>
2020-02-05 09:52:31 -0800

    implementing email service

--- src/main/java/com/rachnicrice/spordering/controllers/EmailController.java
+++ src/main/java/com/rachnicrice/spordering/controllers/EmailController.java
@@ -1,5 +1,8 @@
 package com.rachnicrice.spordering.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +13,17 @@ import java.security.Principal;
 @Controller
 public class EmailController {
 
+    @Autowired
+    JavaMailSender jSender;
+
     @PostMapping("/submit")
     public RedirectView sendOrder (Principal p, Model m) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("rachnicrice@gmail.com");
+        message.setSubject("test email");
+        message.setText("Yaaaay! You got an email!!");
+        jSender.send(message);
 
         return new RedirectView("/mycart?submitted=true");
 
