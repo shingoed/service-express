b83f66701f8ece984fd1b9ed231ff39e75c9ac5f
Rachael Rice <Rachnicrice@gmail.com>
2020-02-04 17:05:01 -0800

    commenting out unfinished code so app will still run

--- src/main/java/com/rachnicrice/spordering/models/email/JavaMailSenderImpl.java
+++ src/main/java/com/rachnicrice/spordering/models/email/JavaMailSenderImpl.java
@@ -12,23 +12,23 @@ import java.util.Properties;
 
 public class JavaMailSenderImpl implements JavaMailSender{
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("my.gmail@gmail.com");
-        mailSender.setPassword("password");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
+//    @Bean
+//    public JavaMailSender getJavaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername("my.gmail@gmail.com");
+//        mailSender.setPassword("password");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
 
     @Override
     public MimeMessage createMimeMessage() {
